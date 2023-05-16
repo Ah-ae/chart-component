75f2731e6e2f11cd55388fad8c114226980b5a76
Ahae KIM <kimaae@gmail.com>
2023-05-16 21:47:25 +0900

    imitate bar chart tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import jsonData from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: false,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = jsonData.map((d) => d.day); //  ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: jsonData.map((d) => d.amount),
+      backgroundColor: "hsl(10, 79%, 65%)",
+    },
+  ],
+};
+
+export default function App() {
+  return <Bar options={options} data={data} />;
 }
-
-export default App;
