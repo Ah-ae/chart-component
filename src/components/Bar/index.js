412491fc03615165d96e8255d35fcebb09ef82d7
Ahae KIM <kimaae@gmail.com>
2023-05-18 13:07:17 +0900

    add dynamic bgColor of Bar based on today's day

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as S from "./style";
 
 function Bar({ label, amount }) {
@@ -9,15 +9,39 @@ function Bar({ label, amount }) {
   const handleMouseLeave = () => {
     setVisibility("hidden");
   };
+
+  const calculateHeight = (amount) => {
+    return `${amount * 3.6}px`;
+  };
+
+  const [isToday, setIsToday] = useState(false);
+  const DAYS = {
+    mon: 1,
+    tue: 2,
+    wed: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+    sun: 7,
+  };
+  useEffect(() => {
+    const today = new Date().getDay(); // 4 => thu
+    setIsToday(DAYS[label] === today);
+  }, [label]);
+
   return (
     <S.Wrapper>
-      <S.Amount visibility={visibility}>{`$${amount}`}</S.Amount>
-      <S.Bar
-        height={"80px"}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      />
-      <p style={{ color: "hsl(28, 10%, 53%)" }}>{label}</p>
+      <S.BarArea>
+        <S.Bar
+          height={() => calculateHeight(amount)}
+          amount={amount}
+          visibility={visibility}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          today={isToday}
+        />
+      </S.BarArea>
+      <S.Label>{label}</S.Label>
     </S.Wrapper>
   );
 }
