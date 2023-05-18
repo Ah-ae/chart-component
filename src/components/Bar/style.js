3865d3a335282cfd7389d9d452eea96a3e702ac8
Ahae KIM <kimaae@gmail.com>
2023-05-18 13:05:32 +0900

    change Amount compo to :before of Bar compo

--- src/components/Bar/style.js
+++ src/components/Bar/style.js
@@ -5,27 +5,44 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+export const BarArea = styled.div`
+  height: 13rem;
+  display: flex;
+  align-items: flex-end;
+`;
 
 export const Bar = styled.div`
-  width: 56px;
+  position: relative;
+  margin: 0 0.5rem;
+  width: 3.5rem;
   height: ${(props) => props.height};
-  background-color: hsl(10, 79%, 65%);
-  border-radius: 6px;
+  background-color: ${(props) =>
+    props.today ? "var(--cyan)" : "var(--soft-red)"};
+  border-radius: 0.375rem;
   cursor: pointer;
 
   :hover {
-    background-color: rgba(236, 119, 95, 0.6);
+    background-color: ${(props) =>
+      props.today ? "var(--cyan-hover)" : "var(--soft-red-hover)"};
+  }
+
+  &:before {
+    content: "${(props) => `$${props.amount}`}";
+    position: absolute;
+    top: -3rem;
+    left: -0.5rem;
+    width: 4.5rem;
+    padding: 0.6rem 0.2rem;
+    margin-bottom: 0.4rem;
+    background-color: var(--dark-brown);
+    color: #fff;
+    border-radius: 0.375rem;
+    text-align: center;
+    visibility: ${(props) => props.visibility};
   }
 `;
 
-export const Amount = styled.p`
-  width: 72px;
-  height: 40px;
-  padding: 0.6rem 0.2rem;
-  margin-bottom: 0.4rem;
-  background-color: hsl(25, 47%, 15%);
-  color: #fff;
-  border-radius: 6px;
-  text-align: center;
-  visibility: ${(props) => props.visibility};
+export const Label = styled.p`
+  margin-top: 0.4rem;
+  color: var(--medium-brown);
 `;
